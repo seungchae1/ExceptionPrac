abd8c98a6926e24e834ddf28f401e68abf66c892
seungchae <s2125@e-mirim.hs.kr>
2023-08-24 14:11:52 +0900

    기본적인 방식으로 에러처리한 코드

--- ExceptionPrac/Program.cs
+++ ExceptionPrac/Program.cs
@@ -13,7 +13,10 @@ namespace ExceptionPrac
             string[] array = { "가", "나" };
             Console.Write("숫자를 입력해주세요.");
             int input = int.Parse(Console.ReadLine());
-            Console.WriteLine("입력한 위치의 값은 '" + array[input]+"'입니다.");
+            if (input < array.Length && input >= 0)
+            {
+                Console.WriteLine("입력한 위치의 값은 '" + array[input] + "'입니다.");
+            }
         }
     }
 }
